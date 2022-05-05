8f0e8049f23eb8c8336dbf1e1def3364b63b98b5
Amir <nightspy2012@gmail.com>
2022-05-05 20:05:42 +0400

    Created base working version

---
+++ config.py
@@ -0,0 +1,26 @@
+from marshmallow import Schema, fields, post_load
+
+
+class BaseConfig(Schema):
+    def __getattr__(self, item):
+        return self.data.get(item)
+    project_id = fields.Str(required = True)
+    dataset_id = fields.Str(required = True)
+    input_table_id = fields.Str(required = True)
+    output_table_id = fields.Str(required = True)
+    output_topic = fields.Str(required = True)
+    input_rows_limit = fields.Int(required = True)
+    ner_service_url = fields.Str(required=True)
+    google_api_key_path = fields.Str(required=True)
+
+    @post_load
+    def make_config(self, data, **kwargs):
+        self.data = data
+        return self
+
+def load_config(config_path= "config.json") -> BaseConfig:
+    with open( config_path, 'r' ) as file:
+        config_str = file.read()
+        config = BaseConfig().loads(config_str)
+
+        return config
